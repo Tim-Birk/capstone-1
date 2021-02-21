deeb5909bd833522fc38a705acb04eef832bbaf4
Tim Birkmire <timbirkmire@yahoo.com>
2021-02-21 18:47:05 -0500

    Add search page with map functionality and search filters

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, render_template, flash
+from flask import Flask, redirect, render_template, flash, session, g
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Search
-from secrets import SECRET_KEY
+from models import db, connect_db, User, SavedSearch
+from forms import RegisterForm, LoginForm
+from secrets import SECRET_KEY, MAPBOX_ACCESS_TOKEN
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///restroom-finder'
@@ -16,9 +18,123 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+CURR_USER_KEY = "curr_user"
+
+@app.before_request
+def add_user_to_g():
+    """If logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+def validate_password(email, password):
+    """Validate user's password and return a boolean of the result"""
+    user = User.authenticate(email, password)
+    if user:
+        return True
+    
+    return False
 
 @app.route("/")
 def root():
-    """Homepage"""
+    """Homepage.  If user is logged in, automatically redirect to search page"""
 
-    return render_template("/base.html")
+    if g.user:
+        return redirect(f"/search")
+
+    return render_template("/landing.html")
+
+@app.route("/search")
+def search_page():
+    """Search page."""
+
+    if not g.user:
+        flash(f'You must be logged in to view this page.', "error")
+        return redirect(f"/login")
+
+    return render_template("/search.html", token=MAPBOX_ACCESS_TOKEN)
+
+@app.route("/register", methods=["GET","POST"])
+def show_register_form():
+    """GET: Show a form that when submitted will register/create a user.
+    
+    POST: Process the registration form by adding a new user. Then redirect to the search page"""
+    
+    if g.user:
+        return redirect(f"/search")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        firstname = form.firstname.data
+        lastname = form.lastname.data
+
+        new_user = User.register(email, password)
+        new_user.email = email
+        new_user.firstname = firstname
+        new_user.lastname = lastname
+
+        db.session.add(new_user)
+        
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+            errorInfo = e.orig.args
+            if 'email' in e.orig.args[0]:
+                form.email.errors.append(f'{email} is already in use.  Please pick another email.')
+            return render_template('register.html', form=form)
+        
+        do_login(new_user)
+        flash(f'Welcome!  Your new account has been created for {new_user.email}', "success")
+        return redirect(f'/search')
+    else:
+        return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET","POST"])
+def login_user():
+    """GET: Show a form that when submitted will login a user. 
+    
+    POST: Authenticates user and adds them to session. Then redirects to their user page"""
+
+    if g.user:
+        return redirect(f"/search")
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.authenticate(email, password)
+        if user:
+            flash(f"Welcome Back, {user.email}!", "success")
+            do_login(user)
+            return redirect(f'/search')
+        else:
+            form.email.errors = ['Invalid email/password.']
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_user():
+    """Log user out by removing them from the session"""
+    do_logout()
+    flash("You are logged out.", "success")
+    return redirect('/login')
+
--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 """Models for Restroom Finder app."""
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+
+bcrypt = Bcrypt()
 
 class User(db.Model):
     """User."""
@@ -15,18 +18,50 @@ class User(db.Model):
     firstname = db.Column(db.String(50), nullable=True)
     lastname = db.Column(db.String(50), nullable=True)
 
-    searches = db.relationship('Search', backref='user', cascade="all, delete", passive_deletes=True)
+    searches = db.relationship('SavedSearch', backref='user', cascade="all, delete", passive_deletes=True)
 
-class Search(db.Model):
-    """Search."""
+    @classmethod
+    def register(cls, email, password):
+        """Register user w/hashed password & return user."""
 
-    __tablename__ = "searches"
+        hashed = bcrypt.generate_password_hash(password)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user w/username and hashed pwd
+        return cls(email=email, password=hashed_utf8)
+
+    @classmethod
+    def authenticate(cls, email, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            # return user instance
+            return user
+        else:
+            return False
+
+    def __repr__(self):
+        """Show info about user."""
+
+        u = self
+        return f"<User - id: {u.id}, username: {u.username},  name: {u.lastname}, {u.firstname}>"
+
+class SavedSearch(db.Model):
+    """Saved Search."""
+
+    __tablename__ = "saved_searches"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer,db.ForeignKey('users.id', ondelete='cascade'))
     name = db.Column(db.String(50), nullable=False)
     use_current_location = db.Column(db.Boolean, nullable=False, default=True)
-    search_string = db.Column(db.Text, nullable=False) #if null indicates use current location first
+    location_search_string = db.Column(db.Text, nullable=False) #if null indicates use current location first
     lon = db.Column(db.Float, nullable=True) #if null indicates use current location first
     lat = db.Column(db.Float, nullable=True) #if null indicates use current location first
     is_default = db.Column(db.Boolean, nullable=False, default=False)
@@ -34,7 +69,6 @@ class Search(db.Model):
     unisex = db.Column(db.Boolean, nullable=False, default=False)
     changing_table = db.Column(db.Boolean, nullable=False, default=False)      
 
-# DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
     """Connect to database."""
 
